8569b30214c525faf13f67e34fcf16cbe399fc00
robertgordon <robertgordon.subscript2@gmail.com>
2019-05-30 14:13:44 -0400

    lab done

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,113 @@ public class Person {
     private String name;
     private int age;
 
+    private String catchPhrase;
+    private String nemesis;
+    private Boolean consul;
+    private String praetorialProvince;
+    private Integer triumphs;
+
     public Person() {
+        // this.name = "Marcus Porcius Cato Major";
+        // this.age = 2253;
+        // this.catchPhrase = "Ceterum censeo Carthaginem esse delendam";
+        // this.nemesis = "Carthage";
+        // this.consul = true;
+        // this.praetorialProvince = "Sardinia";
+        // this.triumphs = 1;
+        // System.out.println(this.catchPhrase);
+
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+
+        this.catchPhrase = "";
+        this.nemesis = "";
+        this.consul = false;
+        this.praetorialProvince = "";
+        this.triumphs = 0;
     }
 
     public Person(int age) {
+        this.name = "";
+        if(age > 0) {
+            this.age = age;
+        }
+        else {
+            this.age = 0;
+        }
     }
 
     public Person(String name) {
+        this.name = name;
+        this.age = Integer.MAX_VALUE;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        if(age > 0) {
+            this.age = age;
+        }
+        else {
+            this.age = 0;
+        }
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        if(age > 0) {
+            this.age = age;
+        }
+        else {
+            this.age = 0;
+        }
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
+
+
+    public void setCatchPhrase(String catchPhrase){
+        this.catchPhrase = catchPhrase;
+    }
+
+    public String getCatchPhrase(){
+        return this.catchPhrase;
+    }
+
+
+    public void setNemesis(String nemesis){ this.nemesis = nemesis; }
+
+    public String getNemesis(){ return this.nemesis;}
+
+
+    public void setConsul(Boolean consul) {this.consul = consul;}
+
+    public Boolean getConsul() {return this.consul;}
+
+
+    public void setPraetorialProvince(String province){this.praetorialProvince = province;}
+
+    public String getPraetorialProvince(){ return this.praetorialProvince;}
+
+
+    public void setTriumphs(Integer triumphs){
+        if(triumphs > 0) {
+            this.triumphs = triumphs;
+        }
+        else {
+            this.triumphs = 0;
+        }
+    }
+
+    public Integer getTriumphs(){ return this.triumphs;}
+
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -95,4 +95,156 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+
+
+
+    @Test
+    public void testSetCatchPhrase(){
+        Person person = new Person();
+        String expected = "Quidquid Latine dictum sit, altum videtur";
+
+        person.setCatchPhrase(expected);
+        String actual = person.getCatchPhrase();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testDefaultCatchPhrase(){
+        Person person = new Person();
+        String expected = "";
+
+        String actual = person.getCatchPhrase();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testSetNemesis(){
+        Person person = new Person();
+        String expected = "The perfidious Latins";
+
+        person.setNemesis(expected);
+        String actual = person.getNemesis();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDefaultNemesis(){
+        Person person = new Person();
+        String expected = "";
+
+        String actual = person.getNemesis();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetConsul(){
+        Person person = new Person();
+        Boolean expected = true;
+
+        person.setConsul(expected);
+        Boolean actual = person.getConsul();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDefaultConsul(){
+        Person person = new Person();
+        Boolean expected = false;
+
+        Boolean actual = person.getConsul();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testGetPraetorialProvince(){
+        Person person = new Person();
+        String expected = "Gallia Narbonensis";
+
+        person.setPraetorialProvince(expected);
+        String actual = person.getPraetorialProvince();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testDefaultPraetorialProvince(){
+        Person person = new Person();
+        String expected = "";
+
+        String actual = person.getPraetorialProvince();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testSetTriumphs(){
+        Person person = new Person();
+        Integer expected = 2;
+
+        person.setTriumphs(expected);
+        Integer actual = person.getTriumphs();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testSetTriumphsNegative(){
+        Person person = new Person();
+        Integer expected = 0;
+
+        person.setTriumphs(-2);
+        Integer actual = person.getTriumphs();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDefaultTriumphs(){
+        Person person = new Person();
+        Integer expected = 0;
+
+        Integer actual = person.getTriumphs();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetAgeNegative(){
+        Person person = new Person();
+        Integer expected = 0;
+
+        person.setAge(-5);
+        Integer actual = person.getAge();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorWithAgeNegative(){
+        Person person = new Person(-10);
+        Integer expected = 0;
+
+        Integer actual = person.getAge();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorWithNameAndAgeNegative(){
+        Person person = new Person("Arius of Alexandria", -34);
+        Integer expected = 0;
+
+        Integer actual = person.getAge();
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
